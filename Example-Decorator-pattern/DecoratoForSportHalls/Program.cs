2e911ed1f4b5a419f55dec5474cf6301dba46d05
DmytroKovinskyi <163335412+DmytroKovinskyi@users.noreply.github.com>
2024-05-08 15:14:43 +0300

    Laba-9

---
+++ Example-Decorator-pattern/DecoratoForSportHalls/Program.cs
@@ -0,0 +1,64 @@
+﻿public interface ISportsHall
+{
+    string Description { get; }
+    decimal Price { get; }
+}
+public class BasicSportsHall : ISportsHall
+{
+    public string Description => "Базовий спортивний зал";
+
+    public decimal Price => 500.00M;
+}
+public abstract class FitnessTrackerDecorator : ISportsHall
+{
+    protected ISportsHall sportsHall;
+
+    public FitnessTrackerDecorator(ISportsHall sportsHall)
+    {
+        this.sportsHall = sportsHall;
+    }
+
+    public abstract string Description { get; }
+
+    public abstract decimal Price { get; }
+}
+public class HeartRateMonitorDecorator : FitnessTrackerDecorator
+{
+    public HeartRateMonitorDecorator(ISportsHall sportsHall) : base(sportsHall)
+    {
+    }
+
+    public override string Description => $"{sportsHall.Description} з пульсометром";
+
+    public override decimal Price => sportsHall.Price + 50.00M;
+}
+
+public class CalorieTrackerDecorator : FitnessTrackerDecorator
+{
+    public CalorieTrackerDecorator(ISportsHall sportsHall) : base(sportsHall)
+    {
+    }
+
+    public override string Description => $"{sportsHall.Description} з трекером калорiй";
+
+    public override decimal Price => sportsHall.Price + 75.00M;
+}
+class Program
+{
+    static void Main(string[] args)
+    {
+        ISportsHall sportsHall = new BasicSportsHall();
+
+        Console.WriteLine($"{sportsHall.Description}: ${sportsHall.Price}");
+
+        sportsHall = new HeartRateMonitorDecorator(sportsHall);
+
+        Console.WriteLine($"{sportsHall.Description}: ${sportsHall.Price}");
+
+        sportsHall = new CalorieTrackerDecorator(sportsHall);
+
+        Console.WriteLine($"{sportsHall.Description}: ${sportsHall.Price}");
+
+        Console.ReadLine();
+    }
+}
---
+++ Example-Decorator-pattern/Example-Decorator-pattern/Program.cs
@@ -0,0 +1,94 @@
+﻿using System;
+
+namespace ExampleDecoratorpattern.DesignPatterns.Composite.Conceptual
+{
+    public abstract class Component
+    {
+        public abstract string Operation();
+    }
+
+    class ConcreteComponent : Component
+    {
+        public override string Operation()
+        {
+            return "Бетонний компонент";
+        }
+    }
+
+    abstract class Decorator : Component
+    {
+        protected Component _component;
+
+        public Decorator(Component component)
+        {
+            this._component = component;
+        }
+
+        public void SetComponent(Component component)
+        {
+            this._component = component;
+        }
+
+        public override string Operation()
+        {
+            if (this._component != null)
+            {
+                return this._component.Operation();
+            }
+            else
+            {
+                return string.Empty;
+            }
+        }
+    }
+
+    class ConcreteDecoratorA : Decorator
+    {
+        public ConcreteDecoratorA(Component comp) : base(comp)
+        {
+        }
+
+        public override string Operation()
+        {
+            return $"Декоратор бетонуА({base.Operation()})";
+        }
+    }
+
+    class ConcreteDecoratorB : Decorator
+    {
+        public ConcreteDecoratorB(Component comp) : base(comp)
+        {
+        }
+
+        public override string Operation()
+        {
+            return $"Декоратор бетонуВ({base.Operation()})";
+        }
+    }
+
+    public class Client
+    {
+        public void ClientCode(Component component)
+        {
+            Console.WriteLine("Результат: " + component.Operation());
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Client client = new Client();
+
+            var simple = new ConcreteComponent();
+            Console.WriteLine("Клiєнт: Я отримую простий компонент:");
+            client.ClientCode(simple);
+            Console.WriteLine();
+
+            ConcreteDecoratorA decorator1 = new ConcreteDecoratorA(simple);
+            ConcreteDecoratorB decorator2 = new ConcreteDecoratorB(decorator1);
+            Console.WriteLine("Клiєнт: Тепер у мене є декорований компонент:");
+            client.ClientCode(decorator2);
+        }
+    }
+}
